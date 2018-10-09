1644b638ba8e7ba1cdf4ddbdcba1e2a73aeb0c48
felipedf <felipe.diogenes13@gmail.com>
2018-10-08 22:23:51 -0300

    Redirect when on /checkout and ingredients is null

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -15,18 +15,23 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ingredients}
-          checkoutContinue={this.handleContinueCheckout}
-          checkoutCancel={this.handleCancelCheckout}
-        />
-      <Route
-        path={this.props.match.path + '/contact-data'}
-        component={ContactData}/>
+    let summary = <Redirect to="/"/>;
+    if (this.props.ingredients) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ingredients}
+            checkoutContinue={this.handleContinueCheckout}
+            checkoutCancel={this.handleCancelCheckout}
+          />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData}
+          />
       </div>
-    );
+      )
+    }
+    return summary;
   }
 }
 
